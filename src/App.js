d61e4231cfeba4295b4f004db015c9d820dcf328
mibu119 <horang296@gmail.com>
2023-06-26 19:01:31 +0900

    👽️ API: detail link 성공

--- src/App.js
+++ src/App.js
@@ -1,35 +1,17 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import {useState, useEffect} from "react";
 import Detail from "./pages/Detail";
 import Header from "./components/Header";
 import MainList from "./MainList";
 
 function App() {
-  const [movies, setMovies] = useState([]);
-
-  useEffect(() => {
-    const getMovies = async () => {
-      fetch(
-        "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year",
-      )
-        .then(res => res.json())
-        .then(json => {
-          console.log(json.data.movies);
-          setMovies(json.data.movies);
-        });
-    };
-    getMovies();
-  }, []);
-  console.log("==========");
-  console.log("movies", movies);
   return (
     <Router>
-    <Header setMovies={setMovies} />
+      <Header />
       <main>
-      <Routes>
-        <Route path="/detail" element={<Detail />} />
-        <Route path="/detail" element={<MainList />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<MainList />} />
+          <Route path="/detail/:movie_id" element={<Detail />} />
+        </Routes>
       </main>
     </Router>
   );
--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,33 +1,41 @@
 import "../styles/Detail.scss";
+import {useLocation} from "react-router-dom";
 
-export default function Detail({movie}) {
+export default function Detail() {
+  const location = useLocation();
+  const movie = location.state;
+  console.log(location);
+  console.log(movie);
+
   return (
     <section className="detail">
-      <img className="poster" src="" alt="영화 포스터"></img>
+      <img className="poster" src={`${movie.poster}`} alt="영화 포스터"></img>
       <section className="description">
         <header>
           <div>
             <h1>
-              {/* {movie.title} */}
+              {movie.title}
               movie.title
             </h1>
             <small>
               <span>
-                {/* {movie.year} */}
+                {movie.year}
                 movie.year
               </span>
               <span>
-                {/* {movie.runtime}  */}
-                movie.runtime
+                {movie.runtime}
+                {/* movie.runtime */}
               </span>
               <span>
-                {/* {movie.genres} */}
-                movie.genres
+                {movie.genres.map(genre => {
+                  return <span>{genre}</span>;
+                })}
+                {/* movie.genres */}
               </span>
             </small>
           </div>
           <div className="rating">
-            {/* {movie.rating} */}
+            {movie.rating}
             movie.rating
           </div>
         </header>
@@ -35,7 +43,7 @@ export default function Detail({movie}) {
           <li>Overview</li>
         </nav>
         <article>
-          {/* {movie.summary} */}
+          {movie.summary}
           movie.summary
         </article>
       </section>
