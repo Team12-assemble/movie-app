dab7844077ea40a6175521a262a51d87651c11bc
lyeejj <lyshine012@gmail.com>
2023-06-30 17:23:27 +0900

    useFetchMovies, pagination 리팩토링

--- src/pages/MainList.js
+++ src/pages/MainList.js
@@ -1,3 +1,4 @@
+import {useState} from "react";
 import MovieList from "../components/MovieList";
 import {useLocation} from "react-router-dom";
 import "../styles/MainList.scss";
@@ -5,57 +6,32 @@ import useFetchMovies from "../hooks/useFetchMovies";
 import Pagination from "../components/Pagination";
 
 function MainList() {
-  const [page, setPage] = useState({
-    count: 0,
-    limit: 0,
-  });
   const [activePage, setActivePage] = useState(1);
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
   const title = queryParams.get("title");
 
-//   const url = `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`;
-//   const searchUrl = `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year&query_term=${title}`;
+  const url = `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`;
+  const searchUrl = `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year&query_term=${title}`;
+  const activeUrl = `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year&page=${activePage}`;
 
-//   const {loading, payload, error} = useFetchMovies(
-//     !title ? url : searchUrl,
-//     title,
-//   );
-//   if (error) console.log(error);
-
-  useEffect(() => {
-    fetch(
-      title
-        ? `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year&query_term=${title}`
-        : activePage
-        ? `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year&page=${activePage}`
-        : `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`,
-    )
-      .then(response => response.json())
-      .then(json => {
-        setMovies(json.data.movies);
-        setLoading(false);
-        setPage({
-          count: json.data.movie_count,
-          limit: json.data.limit,
-        });
-      });
-  }, [activePage, title]);
+  const {loading, payload, error} = useFetchMovies(
+    title ? searchUrl : activePage ? activeUrl : url,
+    title,
+  );
+  if (error) console.log(error);
 
   return (
     <>
       {loading ? (
         <h1>Loading...</h1>
       ) : (
-//         <div className="movie-list">
-//           <MovieList movies={payload.movies} />
-//         </div>
         <>
           <div className="movie-list">
-            <MovieList movies={movies} />
+            <MovieList movies={payload.movies} />
           </div>
           <Pagination
-            page={page}
+            page={{count: payload.movie_count, limit: payload.limit}}
             setActivePage={setActivePage}
             activePage={activePage}
           />
