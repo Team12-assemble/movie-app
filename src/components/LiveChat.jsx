b28a1b0492e759c3e17252ff26ba840544dea729
mibu119 <horang296@gmail.com>
2023-07-05 09:19:22 +0900

    💄 채팅창 닫기 애니메이션 추가

--- src/components/LiveChat.jsx
+++ src/components/LiveChat.jsx
@@ -18,51 +18,49 @@ export default function LiveChat() {
 
   return (
     <>
-      {!isActive ? (
-        <div onClick={handleToggle} className="live_chat_open">
-          <FontAwesomeIcon icon={faMessage} className="live_chat_icon" />
-        </div>
-      ) : (
-        <section className={`live-chat-modal ${isActive ? "on" : ""}`}>
-          <header>
-            <h4>실시간 채팅</h4>
-            <div
-              className={`close ${isActive ? "" : "off"}`}
-              onClick={handleToggle}
-            >
-              <FontAwesomeIcon icon={faXmark} className="close-icon" />
-            </div>
-          </header>
-          <section className="live-chat-body">
-            <ul className="chat-list">
-              {chats.map((chat, i) => {
-                return (
-                  <li className="chat" key={i}>
-                    <label>{chat.username}</label>
-                    <p className="chat-content">{chat.content}</p>
-                    <p className="chat-time">{chat.time}</p>
-                  </li>
-                );
-              })}
-            </ul>
-            <form onSubmit={handleSubmit}>
-              <input
-                required
-                maxLength={30}
-                onChange={e => {
-                  setMessage(e.target.value);
-                }}
-                name="message"
-                value={message}
-                placeholder="무엇이든 물어보세요."
-              />
-              <button className="chat-submit-btn" type="submit">
-                <FontAwesomeIcon icon={faPaperPlane} />
-              </button>
-            </form>
-          </section>
+      <div onClick={handleToggle} className="live_chat_open">
+        <FontAwesomeIcon icon={faMessage} className="live_chat_icon" />
+      </div>
+
+      <section className={`live-chat-modal ${isActive ? "on" : "off"}`}>
+        <header>
+          <h4>실시간 채팅</h4>
+          <div
+            className={`close ${isActive ? "on" : "off"}`}
+            onClick={handleToggle}
+          >
+            <FontAwesomeIcon icon={faXmark} className="close-icon" />
+          </div>
+        </header>
+        <section className="live-chat-body">
+          <ul className="chat-list">
+            {chats.map((chat, i) => {
+              return (
+                <li className="chat" key={i}>
+                  <label>{chat.username}</label>
+                  <p className="chat-content">{chat.content}</p>
+                  <p className="chat-time">{chat.time}</p>
+                </li>
+              );
+            })}
+          </ul>
+          <form onSubmit={handleSubmit}>
+            <input
+              required
+              maxLength={30}
+              onChange={e => {
+                setMessage(e.target.value);
+              }}
+              name="message"
+              value={message}
+              placeholder="무엇이든 물어보세요."
+            />
+            <button className="chat-submit-btn" type="submit">
+              <FontAwesomeIcon icon={faPaperPlane} />
+            </button>
+          </form>
         </section>
-      )}
+      </section>
     </>
   );
 }
